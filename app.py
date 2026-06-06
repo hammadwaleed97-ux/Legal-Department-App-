eb37d6ca3e1a480ef38a8c90460a1bf6fe918522
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-06-06 20:16:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -344,6 +344,88 @@ elif st.session_state.page == "archive":
     """, unsafe_allow_html=True)
 
     st.info("سيتم إضافة الأرشيف فى الجزء القادم")
+elif st.session_state.page == "search":
+
+    st.markdown("""
+    <h2 style='text-align:center;color:white'>
+    🔍 البحث عن دعوى
+    </h2>
+    """, unsafe_allow_html=True)
+
+    search_text = st.text_input(
+        "اكتب رقم الدعوى أو اسم أحد الخصوم"
+    )
+
+    if search_text:
+
+        result = pd.read_sql_query(
+            """
+            SELECT *
+            FROM cases
+            WHERE
+                claimant LIKE ?
+                OR defendant LIKE ?
+                OR case_no LIKE ?
+                OR subject LIKE ?
+            ORDER BY session_date ASC
+            """,
+            conn,
+            params=(
+                f"%{search_text}%",
+                f"%{search_text}%",
+                f"%{search_text}%",
+                f"%{search_text}%"
+            )
+        )
+
+        if result.empty:
+
+            st.warning("لا توجد نتائج")
+
+        else:
+
+            st.success(f"تم العثور على {len(result)} قضية")
+
+            for _, row in result.iterrows():
+
+                title = (
+                    f"{row['claimant']} ضد {row['defendant']} | "
+                    f"دعوى {row['case_no']} لسنة {row['judicial_year']}"
+                )
+
+                with st.expander(title):
+
+                    st.write(
+                        f"الخصوم : {row['claimant']} ضد {row['defendant']}"
+                    )
+
+                    st.write(
+                        f"رقم الدعوى : {row['case_no']}"
+                    )
+
+                    st.write(
+                        f"السنة القضائية : {row['judicial_year']}"
+                    )
+
+                    st.write(
+                        f"المحكمة : {row['court']}"
+                    )
+
+                    st.write(
+                        f"اسم المحكمة : {row['court_name']}"
+                    )
+
+                    st.write(
+                        f"موضوع الدعوى : {row['subject']}"
+                    )
+
+                    st.write(
+                        f"تاريخ الجلسة : {row['session_date']}"
+                    )
+
+                    st.write(
+                        f"الحالة : {row['judgment_result']}"
+                    )    
 
 elif st.session_state.page == "deleted":
 
