a0182df8399010b62e12c5b52112fca7e4135be2
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-06-24 20:35:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -128,3 +128,37 @@ with center:
 # =====================================
 
 st.markdown("<br><br><br><br>", unsafe_allow_html=True)
+# =====================================
+# شريط سفلي ثابت
+# =====================================
+
+st.markdown("""
+<div style="
+position:fixed;
+bottom:0;
+left:0;
+width:100%;
+background:#000814;
+border-top:2px solid gold;
+padding:8px;
+text-align:center;
+font-size:18px;
+font-weight:bold;
+z-index:9999;">
+
+<span style="color:#FFD700;">مع تحيات / وليد حماد</span>
+
+<span style="color:white;"> ⚖️ </span>
+
+<span style="color:#00FFFF;">الإدارة العامة للشئون القانونية</span>
+
+<span style="color:white;"> ⚖️ </span>
+
+<span style="color:#7FFF00;">ديوان عام منطقة البحيرة</span>
+
+<span style="color:white;"> ⚖️ </span>
+
+<span style="color:#FF4500;">الهيئة القومية للتأمين الاجتماعى</span>
+
+</div>
+""", unsafe_allow_html=True)
