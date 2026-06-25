d3fe4ae384f9e25b344396f7069f0ff01968810d
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-06-25 19:04:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -230,43 +230,6 @@ st.markdown("<br><br>", unsafe_allow_html=True)
 # =========================
 # الأيقونات
 # =========================
-
-# =========================
-# الصفحة الرئيسية
-# =========================
-
-if st.session_state.page == "home":
-
-    c1,c2,c3 = st.columns([2,4,2])
-
-    with c2:
-
-        if st.button("⚖️ تسجيل القضايا", use_container_width=True):
-            st.session_state.page = "cases"
-            st.rerun()
-
-        st.markdown("<br>", unsafe_allow_html=True)
-
-        if st.button("📋 الحصر العام", use_container_width=True):
-            st.session_state.page = "inventory"
-            st.rerun()
-
-        st.markdown("<br>", unsafe_allow_html=True)
-
-        st.button("🔔 التنبيهات", use_container_width=True)
-
-        st.markdown("<br>", unsafe_allow_html=True)
-
-        st.button("📊 التقارير", use_container_width=True)
-
-        st.markdown("<br>", unsafe_allow_html=True)
-
-        st.button("🗄️ الأرشيف", use_container_width=True)
-
-        st.markdown("<br>", unsafe_allow_html=True)
-        st.markdown("<br>", unsafe_allow_html=True)
-
-       st.button("📚 المكتبة القانونية", use_container_width=True)
 # =========================
 # الشريط السفلي
 # =========================
