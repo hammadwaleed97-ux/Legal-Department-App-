f88a33e7f90f1cb2186cb4360b8ba6dff3b668f8
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-06-29 13:56:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3175,3 +3175,10 @@ direction:rtl;
                     """
 
                 table += "</table>"
+                                st.markdown(table,unsafe_allow_html=True)
+
+                st.markdown("</div>", unsafe_allow_html=True)
+
+            else:
+
+                st.warning("لا توجد دعاوى مطابقة.")
