7b09b57a1f84de4245f252302cf98a7adcd44b85
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-07-02 14:01:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -141,19 +141,6 @@ def create_word_report(
     r.font.size = Pt(12)
 
     # =================================
-    # شعار الهيئة (يمين)
-    # =================================
-
-    p = head.cell(0, 2).paragraphs[0]
-    p.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
-        try:
-        from docx.shared import Mm
-        run = p.add_run()
-        run.add_picture("logo.png", width=Mm(18))
-    except:
-        pass
-
-    doc.add_paragraph()
         # =================================
     # عنوان التقرير
     # =================================
